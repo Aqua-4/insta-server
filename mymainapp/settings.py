84d4db9f310b13164030507d26ecc7b74f8db994
Aqua-4 <parasharsangle@gmail.com>
2020-06-01 23:50:03 +0530

    ENH: allow all hosts

--- mymainapp/settings.py
+++ mymainapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!ls)8#j%4k8oft*-^(=^g@yg*q&4gzq39vq4mn$%)g4^nfi=c@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.200', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
